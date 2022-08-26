1e64959a383432622a9e74a6ad3aa6999daf97b8
Ignacio Baptista [Desktop] <ibaptista@frba.utn.edu.ar>
2022-08-25 21:15:36 -0300

    Added test request

---
+++ src/main/java/com/tacs/backend/model/TestDTO.java
@@ -0,0 +1,12 @@
+package com.tacs.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestDTO {
+    String name;
+    String jorge;
+    String pedro;
+}
