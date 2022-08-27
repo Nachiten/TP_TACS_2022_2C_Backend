21e9af4f3093b572125c365f3808535c1999e033
Ignacio Baptista <ibaptista@grupomercel.com>
2022-08-27 16:12:25 -0300

    Added basic project strcture

--- src/main/java/com/tacs/backend/controller/TestController.java
+++ src/main/java/com/tacs/backend/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.tacs.backend.controllers;
+package com.tacs.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
