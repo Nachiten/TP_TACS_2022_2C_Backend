33ca5656e98101ccb679b19b77d08cce51d4af79
Ignacio Baptista <ibaptista@grupomercel.com>
2022-09-02 20:35:48 -0300

    Added swaggerUI

--- src/main/java/com/tacs/backend/BackendApplication.java
+++ src/main/java/com/tacs/backend/BackendApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BackendApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(BackendApplication.class, args);
   }
@@ -20,6 +21,9 @@ public class BackendApplication {
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
       }
+      // fruta
     };
+
+    // fruta
   }
 }
