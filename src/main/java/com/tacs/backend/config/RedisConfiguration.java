20ba3ba38f0f7d56799684b47f77f0e4d995d083
Ignacio Baptista <ibaptista@grupomercel.com>
2022-09-02 22:17:48 -0300

    Finished implementing redis DB and docker-compose

--- src/main/java/com/tacs/backend/config/RedisConfiguration.java
+++ src/main/java/com/tacs/backend/config/RedisConfiguration.java
@@ -1,21 +1,34 @@
 package com.tacs.backend.config;
 
 import com.tacs.backend.domain.Match;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
 
+  @Value("${spring.redis.host}")
+  private String REDIS_HOST;
+
+  @Value("${spring.redis.port}")
+  private Integer REDIS_PORT;
+
   @Bean
   JedisConnectionFactory jedisConnectionFactory() {
-    JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+    // JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(REDIS_HOST,
+    // REDIS_PORT);
+
     // se usan los valores por default si no hay que poner los siguientes datos
-    // jedisConnectionFactory.setHostName("localhost");
-    // jedisConnectionFactory.setConvertPipelineAndTxResults(puerto);
-    return new JedisConnectionFactory();
+    //    jedisConnectionFactory.setHostName("localhost");
+    //    jedisConnectionFactory.setConvertPipelineAndTxResults(puerto);
+
+    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
+
+    return new JedisConnectionFactory(config);
   }
 
   @Bean
--- src/main/java/com/tacs/backend/controller/MatchController.java
+++ src/main/java/com/tacs/backend/controller/MatchController.java
@@ -1,14 +1,13 @@
 package com.tacs.backend.controller;
 
 import com.tacs.backend.domain.Match;
-import com.tacs.backend.dto.TestDTO;
 import com.tacs.backend.service.IMatchService;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/matches")
 public class MatchController {
   @Autowired IMatchService matchService;
 
@@ -16,27 +15,22 @@ public class MatchController {
   //    this.matchRepository = matchRepository;
   // }
 
-  @GetMapping("/health")
-  public TestDTO health() {
-    return new TestDTO("Healthy!");
-  }
-
-  @GetMapping("/matches")
+  @GetMapping("/buscar")
   public Map<String, Match> matchgridview() {
     return matchService.verPartidos();
   }
 
-  @GetMapping("/matches/{id}")
+  @GetMapping("/{id}")
   public Match buscarUnPartido(@PathVariable String id) {
     return matchService.buscarUnPartido(id);
   }
 
-  @PostMapping(value = "/matches")
+  @PostMapping()
   public void createMatch(@RequestBody Match match) {
     matchService.crearPartido(match);
   }
 
-  @DeleteMapping("/matches/{id}")
+  @DeleteMapping("/{id}")
   public void borrarPartido(@PathVariable String id) {
     matchService.borrarPartido(id);
   }
